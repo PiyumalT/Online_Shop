85889a184d42383aa4b31b104908191158b44757
Tharindu Piyumal <94281006+PiyumalT@users.noreply.github.com>
2023-01-13 11:56:04 +0530

    Add files via upload

--- online_shop.sql
+++ online_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 09, 2023 at 06:15 PM
+-- Generation Time: Jan 13, 2023 at 06:24 AM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `address` (
   `zip_code` varchar(8) NOT NULL,
   `phone` varchar(15) NOT NULL,
   PRIMARY KEY (`address_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `address`
@@ -71,7 +71,21 @@ INSERT INTO `address` (`address_id`, `user_id`, `name`, `line_1`, `line_2`, `cit
 (21, 1, 'my namex', 'ad 1cd', '', 'cty 1 b', 'pr 1', 'sl2', '0001', '7771234567'),
 (22, 1, 'my namex', 'ad 1cd', '', 'cty 1 b', 'pr 1', 'sl2', '0001', '7771234561616'),
 (23, 1, 'my namex', 'ad 1cd', 'yfyvfy', 'cty 1 b', 'pr 1', 'sl2', '0001', '7771234561616'),
-(24, 1, 'my namexaaa', 'ad 1cd', 'yfyvfy', 'cty 1 b', 'pr 1', 'sl2', '0001', '7771234561616');
+(24, 1, 'my namexaaa', 'ad 1cd', 'yfyvfy', 'cty 1 b', 'pr 1', 'sl2', '0001', '7771234561616'),
+(25, 13, 'fdsgsdgfds', 'fasfas', 'fasfasf', 'asfasfas', 'asfasfasfas', 'asfasfas', '4444646', '596494964'),
+(26, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(27, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(28, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(29, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(30, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(31, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(32, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(33, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(34, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(35, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(36, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(37, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623'),
+(38, 15, 'ffhggh', 'ghfghfg', 'fghgf', 'fghfg', 'ghfghgf', 'ghfghfg', 'fghfg', '1616262623');
 
 -- --------------------------------------------------------
 
@@ -105,15 +119,19 @@ CREATE TABLE IF NOT EXISTS `cart` (
   `item_id` int(11) NOT NULL,
   `options` varchar(15) NOT NULL,
   `qty` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`item_id`,`options`)
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`item_id`,`options`,`qty`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cart`
 --
 
-INSERT INTO `cart` (`user_id`, `item_id`, `options`, `qty`) VALUES
-(1, 1, 'red', 1);
+INSERT INTO `cart` (`user_id`, `item_id`, `options`, `qty`, `price`) VALUES
+(1, 1, 'red', 1, NULL),
+(12, 1, 'blue', 1, NULL),
+(13, 1, 'blue', 1, 5000),
+(13, 1, 'yellow', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -131,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `item` (
   `qty` int(8) NOT NULL,
   `category` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`item_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `item`
@@ -139,7 +157,11 @@ CREATE TABLE IF NOT EXISTS `item` (
 
 INSERT INTO `item` (`item_id`, `name`, `description`, `price`, `options`, `qty`, `category`) VALUES
 (1, 'sample toy car', 'remote control toy car for children and adults. brand-new toy car. rechargeable battery up to 5 hours works', 2500, 'red, blue, green, yellow, black, white, gray, random', 50, NULL),
-(0, 'Sorry, invalid Item.', 'Unavailable item', 0, 'default ', 0, NULL);
+(0, 'Sorry, invalid Item.', 'Unavailable item', 0, 'default ', 0, NULL),
+(3, 'rtgrgr', 'fasfsa', 555, 'safasf', 563, 'dsfgsdg'),
+(4, 'rtgrgr', 'fasfsa', 555, 'safasf', 563, 'dsfgsdg'),
+(5, 'rtgrgr', 'fasfsa', 555, 'safasf', 563, 'dsfgsdg'),
+(6, 'rtgrgr', 'fasfsa', 555, 'safasf', 563, 'dsfgsdg');
 
 -- --------------------------------------------------------
 
@@ -157,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `paid` int(5) DEFAULT NULL,
   `shipped` int(11) DEFAULT NULL,
   PRIMARY KEY (`order_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `orders`
@@ -174,7 +196,46 @@ INSERT INTO `orders` (`order_id`, `cus_id`, `address_id`, `price`, `date`, `paid
 (8, 1, 21, NULL, NULL, NULL, NULL),
 (9, 1, 22, NULL, NULL, NULL, NULL),
 (10, 1, 23, 22500, '2023-01-09 12:18:47', 1, NULL),
-(11, 1, 24, NULL, NULL, NULL, NULL);
+(11, 1, 24, NULL, NULL, NULL, NULL),
+(12, 1, NULL, NULL, NULL, NULL, NULL),
+(13, 1, NULL, NULL, NULL, NULL, NULL),
+(14, 1, NULL, NULL, NULL, NULL, NULL),
+(15, 1, NULL, NULL, NULL, NULL, NULL),
+(16, 1, NULL, NULL, NULL, NULL, NULL),
+(31, 15, NULL, NULL, NULL, NULL, NULL),
+(18, 1, NULL, NULL, NULL, NULL, NULL),
+(19, 1, 25, 20000, '2023-01-09 19:25:57', 1, NULL),
+(20, 1, 26, NULL, NULL, NULL, NULL),
+(21, 1, 28, 10000, '2023-01-11 16:09:07', 1, NULL),
+(32, 15, NULL, NULL, NULL, NULL, NULL),
+(23, 1, 30, NULL, NULL, NULL, NULL),
+(33, 15, NULL, NULL, NULL, NULL, NULL),
+(28, 15, NULL, NULL, NULL, NULL, NULL),
+(29, 15, NULL, NULL, NULL, NULL, NULL),
+(30, 15, NULL, NULL, NULL, NULL, NULL),
+(34, 15, NULL, NULL, NULL, NULL, NULL),
+(35, 15, NULL, NULL, NULL, NULL, NULL),
+(36, 15, NULL, NULL, NULL, NULL, NULL),
+(37, 15, NULL, NULL, NULL, NULL, NULL),
+(38, 15, NULL, NULL, NULL, NULL, NULL),
+(39, 15, NULL, NULL, NULL, NULL, NULL),
+(40, 15, NULL, NULL, NULL, NULL, NULL),
+(41, 15, NULL, NULL, NULL, NULL, NULL),
+(42, 15, NULL, NULL, NULL, NULL, NULL),
+(43, 15, NULL, NULL, NULL, NULL, NULL),
+(44, 15, NULL, NULL, NULL, NULL, NULL),
+(45, 15, NULL, NULL, NULL, NULL, NULL),
+(46, 15, NULL, NULL, NULL, NULL, NULL),
+(47, 15, NULL, NULL, NULL, NULL, NULL),
+(48, 15, 31, 7500, '2023-01-11 19:29:31', 1, NULL),
+(49, 15, 32, NULL, NULL, NULL, NULL),
+(50, 15, 34, 7500, '2023-01-12 10:29:22', 1, NULL),
+(51, 15, NULL, NULL, NULL, NULL, NULL),
+(52, 15, 35, NULL, NULL, NULL, NULL),
+(53, 15, 36, 22500, '2023-01-12 11:10:00', 1, NULL),
+(54, 15, NULL, NULL, NULL, NULL, NULL),
+(55, 15, 37, 10000, '2023-01-12 12:45:53', 1, NULL),
+(56, 15, 38, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -208,7 +269,45 @@ INSERT INTO `order_details` (`order_id`, `item_id`, `qty`, `options`, `price`) V
 (8, 1, 1, 'white', 2500),
 (9, 1, 2, 'green', 5000),
 (10, 1, 9, 'yellow', 22500),
-(11, 1, 4, 'green', 10000);
+(11, 1, 4, 'green', 10000),
+(12, 1, 1, 'blue', 2500),
+(13, 1, 1, 'blue', 2500),
+(14, 1, 1, 'blue', 2500),
+(15, 1, 1, 'blue', 2500),
+(16, 1, 1, 'blue', 2500),
+(17, 1, 1, 'blue', 2500),
+(18, 1, 1, 'blue', 2500),
+(19, 1, 1, 'blue', 2500),
+(20, 1, 4, 'red', 10000),
+(21, 1, 4, 'red', 10000),
+(22, 1, 1, 'red', 2500),
+(23, 1, 1, 'red', 2500),
+(25, 1, 50, 'black', 125000),
+(26, 1, 50, 'black', 125000),
+(27, 1, 50, 'black', 125000),
+(28, 1, 3, 'gray', 7500),
+(29, 1, 3, 'gray', 7500),
+(30, 1, 3, 'gray', 7500),
+(31, 1, 6, 'gray', 15000),
+(46, 1, 7, 'gray', 17500),
+(46, 1, 5, 'random', 12500),
+(47, 1, 1, 'gray', 2500),
+(47, 1, 2, 'random', 5000),
+(48, 1, 1, 'gray', 2500),
+(48, 1, 2, 'random', 5000),
+(49, 1, 2, 'gray', 5000),
+(49, 1, 2, 'random', 5000),
+(50, 1, 1, 'gray', 2500),
+(50, 1, 2, 'random', 5000),
+(51, 1, 4, 'blue', 10000),
+(51, 1, 1, 'gray', 2500),
+(51, 1, 2, 'random', 5000),
+(53, 1, 4, 'blue', 10000),
+(53, 1, 5, 'blue', 12500),
+(54, 1, 4, 'blue', 10000),
+(55, 1, 4, 'red', 10000),
+(56, 1, 2, 'green', 5000),
+(56, 1, 8, 'red', 20000);
 
 -- --------------------------------------------------------
 
@@ -275,7 +374,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `reg_date` date NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -292,7 +391,11 @@ INSERT INTO `users` (`id`, `email`, `password`, `Name`, `reg_date`) VALUES
 (8, 'aaasa@aa.aaa', 'aaaaa', NULL, '2023-01-04'),
 (9, 'aadd@aa.ff', 'aaaaaa', NULL, '2023-01-04'),
 (10, 'gdfg@dsgfd.fg', 'aaaaaaaa', NULL, '2023-01-04'),
-(11, 'gfdg@dsgsdf.fdg', 'aaaaaa', NULL, '2023-01-04');
+(11, 'gfdg@dsgsdf.fdg', 'aaaaaa', NULL, '2023-01-04'),
+(12, 'sadas@fds.ad', 'aaaaaaa', NULL, '2023-01-09'),
+(13, 'fdfgsd@dsgds.rfg', 'aaaaaa', NULL, '2023-01-09'),
+(14, 'ddsdsds@ffd.ga', 'aaaaaaa', NULL, '2023-01-10'),
+(15, 'aa@aa.aa', 'AAAaaa123', NULL, '2023-01-10');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
