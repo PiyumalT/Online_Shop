89a8161109578f6b3eae7e8b77c3f7bba0826ccc
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-01-15 18:07:46 +0530

    add hero image section + carousel effect to hero image.

--- home_page.php
+++ home_page.php
@@ -13,6 +13,9 @@ include_once("connect.php");
     <link rel="stylesheet" href="./css/nav_bar.css">
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/home_page.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
 </head>
 <body>
 
@@ -52,43 +55,57 @@ create_header();
 
 <main>
     <div class="hero-img">
+        <img src="" alt="">
     </div>
 </main>
 <section class="popular category">
     <h2 class="category-tag">Popular</h2>
-    <button class="left handle"></button>
+    <!--    <button class="left-btn .hide"><i class="fa-solid fa-arrow-left"></i></button>-->
     <div class="image-container">
         <?php
         for ($i = 0; $i < 5; $i++) {
-            echo "<div class='category-img'><img src='item-image.jpg' alt='item-$i'></div>";
+            echo "<div class='category-img'> <img src='./item-image.jpg' alt='item-$i'></div>";
         }
         ?>
     </div>
+    <!--    <button class="right-btn"><i class="fa-solid fa-arrow-right"></i></button>-->
 </section>
 <section class="latest category">
     <h2 class="category-tag">Latest</h2>
-    <button class="left handle"></button>
+    <!--    <button class="left-btn .hide"><i class="fa-solid fa-arrow-left"></i></button>-->
     <div class="image-container">
         <?php
         for ($i = 0; $i < 5; $i++) {
-            echo "<div class='category-img'><img src='item-image.jpg' alt='item-$i'></div>";
+            echo "<div class='category-img'><img src='./item_pics/6.jpg' alt='item-$i'></div>";
         }
         ?>
     </div>
+    <!--    <button class="right-btn"><i class="fa-solid fa-arrow-right"></i></button>-->
 </section>
 <script>
     // change hero img
     let i = 0;
+    const imgChangeTime = 5000;
+    const imageRemoveTime = 500;
 
     function changeHeroImg() {
-        const colorArray = ["red", "pink", "black"]; //php retrieve here
-        heroImg.style.background = colorArray[colorArray[i] % colorArray.length];
-        i = (i + 1) % colorArray.length;
-        heroImg.style.background = colorArray[i];
+        const imgArray = ['./item-image.jpg', "./item_pics/6.jpg"]; //php retrieve here
+        const imgElement = document.createElement("img");
+        imgElement.src = imgArray[i];
+        imgElement.classList.add("slide-out");
+        imgElement.style.animationDelay = `${imgChangeTime - imageRemoveTime}ms`;
+        imgElement.style.animationDuration = `${imageRemoveTime}ms`;
+        imgElement.style.aspectRatio = `${imgElement.width} / ${imgElement.height}`
+        i = (i + 1) % imgArray.length;
+        heroImg.querySelector("img").remove();
+        heroImg.innerHTML = "";
+        heroImg.append(imgElement);
     }
 
     const heroImg = document.querySelector("main");
-    setInterval(changeHeroImg, 5000);
+    changeHeroImg();
+    setInterval(changeHeroImg, imgChangeTime);
 </script>
+<!--<script src="./js/home_page_carousel.js"></script>-->
 </body>
 </html>
