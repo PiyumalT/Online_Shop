06e895aef37d4cdc509c1dbc23a978b26d6170cb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-01-16 22:10:50 +0530

    remove collision of user_page.js and remove_effect_of_invalid_and_valid_css_effect.js.

--- js/remove_effect_of_invalid_and_valid_css_effect.js
+++ js/remove_effect_of_invalid_and_valid_css_effect.js
@@ -1,6 +1,5 @@
-if (inputs == null) {
-    let inputs = document.querySelectorAll("input");
-}
+let inputs = null;
+inputs = document.querySelectorAll("input");
 for (let i = 0; i < inputs.length; i++) {
     inputs[i].style.borderBottom = "1px solid black";
     inputs[i].addEventListener("click", () => {
---
+++ js/user_page.js
@@ -0,0 +1,31 @@
+let inputs = document.getElementsByTagName("input");
+
+function triggerEditMode(e) {
+    if (e.dataset.inEditMode === "false") {
+        e.innerText = "Exit Edit Mode.";
+        e.dataset.inEditMode = "true";
+        document.querySelector(".button-section").style.display = "flex";
+
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].disabled = false;
+        }
+        document.querySelector("label[for=\"re-type-password\"]").classList.remove("hide");
+    } else {
+        e.innerText = "Edit Details.";
+        e.dataset.inEditMode = "false";
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].disabled = true;
+        }
+        document.querySelector("label[for=\"re-type-password\"]").classList.add("hide");
+        document.querySelector(".button-section").style.display = "none";
+        ResetDetails();
+    }
+}
+
+function ResetDetails() {
+    document.forms[0].reset();
+}
+
+function submitDetails() {
+    document.forms[0].submit();
+}
