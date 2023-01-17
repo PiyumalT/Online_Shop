fef7d9b07d6e872d0e5f968f8c7b266befd5f8a4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-01-17 13:38:37 +0530

    fix fw bugs.

--- admin_dashbord.php
+++ admin_dashbord.php
@@ -5,68 +5,80 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>admin</title>
-    <link rel="stylesheet" href="css/admin_dashbord.css">
+    <link rel="stylesheet" href="css/admin_dashboard.css">
     <?php include 'connect.php';
-      $url='site_img/s-l1600.jpg';  ?>
+    $url = 'site_img/s-l1600.jpg'; ?>
+    <style>
+        body {
+            background-image: url('<?php echo $url ?>');
+        }
+    </style>
 </head>
 <body>
 
-    <style type="text/css">
-    body
-    {
-    background-image:url('<?php echo $url ?>');
-    }
-    </style>
-    
-    <div class="middle">
+<div class="middle">
 
-        <div class="first">
-          <a  href="additem.php"><h2>Add product</h2></a>
-          <a  href="Product_list.php"><h2>Product list</h2></a>
-          <a  href="Sales_list.php"><h2>Sales list</h2></a>
-          <a  href="Users_list.php"><h2>Users list</h2></a>
-        </div>
-
-        <div class="second">
-         <div class="value"><h1>Today orders</h1>
-            <div class="total"> 
-                <?php  $query="SELECT * FROM orders WHERE day(date)=9 && month(date)=1 && year(date)=2023"; 
-                       $result= mysqli_query($connect,$query);
-                       if($result){echo "<h1>";echo mysqli_num_rows($result);echo "</h1>";} 
-                ?>
-            </div>
-         </div>
-         <div class="value"><h1>Today income</h1>
-            <div class="total"> 
-                <?php  $query="SELECT sum(price) FROM orders WHERE day(date)=9 && month(date)=1 && year(date)=2023"; 
-                       $result= mysqli_query($connect,$query);
-                       $r=mysqli_fetch_assoc($result);
-                       if($result){echo "<h1>";echo $r['sum(price)'] ;echo "</h1>";}
-                ?>
-            </div>
-         </div>
-         <div class="value"><h1>Total users</h1>
-            <div class="total"> 
-                <?php  $query="SELECT * FROM users"; 
-                       $result= mysqli_query($connect,$query);
-                       if($result){echo "<h1>";echo mysqli_num_rows($result);echo "</h1>";} 
-                ?>
-            </div>
-         </div>
-         <div class="value"><h1>Total items</h1>
-            <div class="total"> 
-                <?php  $query="SELECT * FROM item"; 
-                       $result= mysqli_query($connect,$query);
-                       if($result){echo "<h1>"; echo mysqli_num_rows($result); echo "</h1>";} 
-                ?>
-            </div>
-         </div>
-        </div>
-      
+    <div class="first">
+        <a href="additem.php"><h2>Add product</h2></a>
+        <a href="Product_list.php"><h2>Product list</h2></a>
+        <a href="Sales_list.php"><h2>Sales list</h2></a>
+        <a href="Users_list.php"><h2>Users list</h2></a>
     </div>
 
-  
+    <div class="second">
+        <div class="value"><h1>Today orders</h1>
+            <div class="total">
+                <?php $query = "SELECT * FROM orders WHERE day(date)=9 && month(date)=1 && year(date)=2023";
+                $result = mysqli_query($connect, $query);
+                if ($result) {
+                    echo "<h1>";
+                    echo mysqli_num_rows($result);
+                    echo "</h1>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="value"><h1>Today income</h1>
+            <div class="total">
+                <?php $query = "SELECT sum(price) FROM orders WHERE day(date)=9 && month(date)=1 && year(date)=2023";
+                $result = mysqli_query($connect, $query);
+                $r = mysqli_fetch_assoc($result);
+                if ($result) {
+                    echo "<h1>";
+                    echo $r['sum(price)'];
+                    echo "</h1>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="value"><h1>Total users</h1>
+            <div class="total">
+                <?php $query = "SELECT * FROM users";
+                $result = mysqli_query($connect, $query);
+                if ($result) {
+                    echo "<h1>";
+                    echo mysqli_num_rows($result);
+                    echo "</h1>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="value"><h1>Total items</h1>
+            <div class="total">
+                <?php $query = "SELECT * FROM item";
+                $result = mysqli_query($connect, $query);
+                if ($result) {
+                    echo "<h1>";
+                    echo mysqli_num_rows($result);
+                    echo "</h1>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 
-    
+</div>
+
+
 </body>
 </html>
